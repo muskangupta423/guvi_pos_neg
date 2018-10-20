ae4c6d3c3a0e634d9b274c2b2393584c8fb301fb
Muskan Gupta <32198462+muskangupta423@users.noreply.github.com>
2018-10-20 04:11:24 -0700

    minimum element for an array

---
+++ min_ele.py
@@ -0,0 +1,12 @@
+n=int(input())
+l=list(map(str,input().split()))
+m=l[0]
+m=int(m)
+for i in range(len(l)):
+	if(l[i].isalpha()):
+		print('Invalid Input')
+		exit()		
+	else:
+		if(int(l[i])<m):
+			m=int(l[i])
+print(m)				
